557fb4a27b7ee08e532c65a385fa751b0695a2d8
Niklas Paulsson <niklasp@gmail.com>
2013-03-11 22:46:23 +0100

    Extended testcase and buggfix

--- testcase.sql
+++ testcase.sql
@@ -1,24 +1,33 @@
 
 SELECT DECODE(1 /* , () ' hello () ' */ , 1 , 'Return a string )' , 'else return something with ticks and a parenthesis '')''') FROM DUAL;
 
+select decode /*(*/ (1 , 1 , 'what') from dual;
+
 SELECT
   -- THIS DECODE( statement should not be included
 DECODE( 1 , 1 , 'But this one should' )
 FROM DUAL;
 
-/* And here are a comment
+/* 
 
    /* 
       and a nested comment , lets try some DECODE( 
    */ 
-
+  And another decode( 'with unbalanced parenthesis' , 'inside a comment' , 'should not be converted'
 */
 
-SELECT DECODE( DECODE( 1 , 1 , 'y0 DaWg , I heard you like decodes') , 
- -- Comment/newlines shoule be ignored
- '' , 'foo' , ' - so I put a decode in your decode)' ) FROM DUAL;
+SELECT 
+DECODE( 
 
-SELECT DECODE( NVL(a,1) , 1 , 'foo', 2 , NVL(b,'bar') ) FROM DUAL;
+       DECODE( 1 , 1 , '( phony retval' ) , 
+
+ -- Comment/newlines shoule be ignored
+
+       '' , '' , 'y0 dAwG - I heard you liked decoeds - so I put a decode in your decode' 
+       ) 
+FROM DUAL;
+
+SELECT DECODE( NVL(1,1) , 1 , 'foo', 2 , NVL('','bar') ) FROM DUAL;
 
 /* Trailing comment */
 
